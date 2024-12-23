cac3dfd077be7129bf4497b146a20a498f3eabc8
db-0 <dan@dbsphere.net>
2024-12-22 21:03:51 -0700

    added player class

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        # Limit FPS to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
